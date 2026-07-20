47293f716db623271855f896925a25eea2f0365c
vnpcoders <vasudevpatidar52@gmail.com>
2026-07-20 08:30:06 +0000

    Jul 20, 2026, 2:00 PM

--- LLM Project/fucherbusiness.py
+++ LLM Project/fucherbusiness.py
@@ -26,7 +26,6 @@ def career_advice(filled, amount, profit,timeperiod):
     4. The best way to build a stable business
     5. The best way to connect and find clients 
     6. The best way to prepare for interviews
-    
     7. Any other tips or advice to help me succeed in my life
     8. Suggest me a weekly schedule to follow to achieve my goal in the given time
     """
