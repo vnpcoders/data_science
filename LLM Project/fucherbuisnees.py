949375f74b239643ce255d64c6cafbb7db9e2f32
vnpcoders <vasudevpatidar52@gmail.com>
2026-07-20 05:46:59 +0000

    Jul 20, 2026, 11:16 AM

--- LLM Project/fucherbuisnees.py
+++ LLM Project/fucherbuisnees.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 # ✅ Page title and header
 
-st.title("🎓 AI  Path Advisor")
+st.title("🎓 AI Fucher buisne Path Advisor")
 st.write("Fill in your preferences and get a personalized career roadmap!")
