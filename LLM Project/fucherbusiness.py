13db981fc5a42c556c5701cd8cd91e38e8d7ca50
vnpcoders <vasudevpatidar52@gmail.com>
2026-07-20 06:00:19 +0000

    Jul 20, 2026, 11:30 AM

--- LLM Project/fucherbusiness.py
+++ LLM Project/fucherbusiness.py
@@ -15,7 +15,7 @@ def career_advice(filled, Amount, profit):
     prompt = f"""
     your are my personal AI assistant that helps me with Bast Businessadvice.
     I'm planning to start a career in {filled}
-    My goal is to become a {career}
+    My goal is to become a {A}
     I want to pursue a course in {course}
     I want to achieve this goal in {timeperiod}
     
