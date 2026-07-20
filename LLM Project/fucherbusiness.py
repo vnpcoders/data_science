bc113446ad913a4e33f48dcf6c14ad468352e2b8
vnpcoders <vasudevpatidar52@gmail.com>
2026-07-20 05:49:20 +0000

    Jul 20, 2026, 11:19 AM

--- LLM Project/fucherbusiness.py
+++ LLM Project/fucherbusiness.py
@@ -6,4 +6,7 @@ import streamlit as st
 st.title("🎓 AI Fucher Business Path Advisor")
 st.write("Fill in your preferences and get a Business roadmap!")
 
+#API Configuration
+genai.configure(api_key="Enter your API key")
+model = genai.GenerativeModel('gemini-1.5-flash')
 
