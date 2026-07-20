0cb53faed694bdc4c54858c2392b2a202640e20b
vnpcoders <vasudevpatidar52@gmail.com>
2026-07-20 05:55:47 +0000

    Jul 20, 2026, 11:25 AM

--- NPl/CAREER_PROJECTR.py
+++ NPl/CAREER_PROJECTR.py
@@ -14,7 +14,7 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 def career_advice(tech, career, course, timeperiod):
     prompt = f"""
     your are my personal AI assistant that helps me with career advice.
-    I'm planning to start a Business in {fill}
+    I'm planning to start a Business in {filled}
     My goal is to become a {career}
     I want to pursue a course in {course}
     I want to achieve this goal in {timeperiod}
