6a2f9060f844a85dccc0207c1e53d7a94c16704a
vnpcoders <vasudevpatidar52@gmail.com>
2026-07-20 08:34:41 +0000

    Jul 20, 2026, 2:04 PM

--- LLM Project/fucherbusiness.py
+++ LLM Project/fucherbusiness.py
@@ -35,6 +35,6 @@ def career_advice(filled, amount, profit,timeperiod):
 
 # Streamlit UI
 filled = st.selectbox("Select Filled ", ["Farming Related", "IT Related", "Marketing Related", "food Related", " textile Related"])
-amount = st.text_input("Enter Amount to invest (e.g. 1 lack, ML Engineer, Web Developer)")
+amount = st.text_input("Enter Amount to invest (e.g. 10000, ML Engineer, Web Developer)")
 profit = st.selectbox("Select minimum profit per month ", ["Bootcamp", "Certification", "Degree", "Self-paced Learning"])
 timeperiod = st.selectbox("Select Time Period", ["3 months", "6 months", "1 year", "2 years"])
